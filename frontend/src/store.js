aa8a02acb9767a81fda36c907bf6c38010113df8
kamlesh <kamlesh.merugu30@gmail.com>
2021-11-29 15:31:28 +0800

    Send Stripe API to Frontend

---
+++ frontend/src/store.js
@@ -0,0 +1,33 @@
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension'
+import { productsReducer,productDetailsReducer } from './reducers/productReducers';
+import { authReducer,userReducer,forgotPasswordReducer } from './reducers/userReducers';
+import { cartReducer } from './reducers/cartReducers';
+
+const reducer = combineReducers({
+
+    products: productsReducer,
+    productDetail : productDetailsReducer,
+    auth:authReducer,
+    user:userReducer,
+    forgotPassword: forgotPasswordReducer,
+    cart:cartReducer
+})
+
+let initialState = {
+    cart: {
+        cartItems: localStorage.getItem('cartItems')
+            ? JSON.parse(localStorage.getItem('cartItems'))
+            : [],
+        shippingInfo: localStorage.getItem('shippingInfo')
+            ? JSON.parse(localStorage.getItem('shippingInfo'))
+            : {}
+    }
+}
+
+const middlware = [thunk];
+
+const store = createStore(reducer, initialState, composeWithDevTools(applyMiddleware(...middlware)))
+
+export default store;
